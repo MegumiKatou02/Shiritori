2cc213235941c6bdb113b94c7de50ee026d672d2
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-03-31 06:45:03 +0700

    update API base URL fallback to use the Vietnamese dictionary API

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     
     setIsLoading(true);
     try {
-      const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api-viet' // || 'https://vietnamese-dictionary-api.vercel.app';
+      const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://vietnamese-dictionary-api.vercel.app';
 
       const response = await axios.get(`${API_BASE_URL}/api/search`, {
         params: {

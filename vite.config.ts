205852b5778c173ea67a266a422a96a11315d601
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-03-31 06:13:07 +0700

    update project configuration for deployment and modify index title and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Shiritori/',
   server: {
     proxy: {
       '/api-viet': {
